b913ed94f206c79a89cfd887f16a7ff81bf8becc
Igor <igorkono343@gmail.com>
2022-12-06 23:05:39 +0200

    fix factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,11 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use Illuminate\Database\Seeder;
-
-use \App\Models\User;
 use App\Models\City;
+use \App\Models\User;
+use App\Models\Photo;
 use \App\Models\History;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         City::factory(100)->create();
         User::factory(10)->create();
         History::factory(200)->create();
+        Photo::factory(50)->create();
     }
 }
